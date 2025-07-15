34c3b122123ff94edcabebdb56c23d0f90a97d08
Asjad_01 <147026784+Asjad128@users.noreply.github.com>
2025-07-15 23:36:42 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,3 @@
 SHEET_KEY = "1fkllLw5dIi9frCz0FV19PJinZd9-Yc3wG51FEyYkS_Q"
-CREDENTIALS_PATH = r"ISBN-FastApi-Py/app/core/testdata.json"
+CREDENTIALS_PATH = r"https://raw.githubusercontent.com/Asjad128/ISBN-FastApi-Py/refs/heads/main/app/core/testdata.json"
 ROWS_PER_SHEET = 800
