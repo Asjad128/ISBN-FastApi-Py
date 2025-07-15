3e253280af6e028998952915e1fa51e89c49ed9e
Asjad_01 <147026784+Asjad128@users.noreply.github.com>
2025-07-15 15:39:04 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,3 @@
 SHEET_KEY = "1fkllLw5dIi9frCz0FV19PJinZd9-Yc3wG51FEyYkS_Q"
-CREDENTIALS_PATH = r"C:\Users\MAGA\Documents\isbn-fastapi\app\core\testdata.json"
+CREDENTIALS_PATH = r"ISBN-FastApi-Py/app/core/testdata.json"
 ROWS_PER_SHEET = 800
