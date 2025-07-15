127df8107221376976cf8359896c3828a617f10b
Asjad_01 <147026784+Asjad128@users.noreply.github.com>
2025-07-15 23:42:10 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,9 @@
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Build the path to the JSON file
+json_path = os.path.join(BASE_DIR, 'testdata.json')
+
 SHEET_KEY = "1fkllLw5dIi9frCz0FV19PJinZd9-Yc3wG51FEyYkS_Q"
-CREDENTIALS_PATH = r"https://raw.githubusercontent.com/Asjad128/ISBN-FastApi-Py/refs/heads/main/app/core/testdata.json"
+CREDENTIALS_PATH = json_path
 ROWS_PER_SHEET = 800
