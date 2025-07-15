6f445eab859929f03470eee42d9b90fca3350375
Asjad_01 <147026784+Asjad128@users.noreply.github.com>
2025-07-16 00:17:15 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,9 +1,4 @@
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Build the path to the JSON file
-json_path = os.path.join(BASE_DIR, 'testdata.json')
 
 SHEET_KEY = "1fkllLw5dIi9frCz0FV19PJinZd9-Yc3wG51FEyYkS_Q"
-CREDENTIALS_PATH = json_path
+CREDENTIALS_PATH = "/etc/secrets/service_account.json"
 ROWS_PER_SHEET = 800
