89a51654204e5bcdfcf1179df953afc04fec9b2b
Asjad_01 <147026784+Asjad128@users.noreply.github.com>
2025-07-16 12:16:12 +0530

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
 
-SHEET_KEY = "1fkllLw5dIi9frCz0FV19PJinZd9-Yc3wG51FEyYkS_Q"
+SHEET_KEY = "1fx8q2oj1zMjvbQghPV3Ty8k9UNfH8lMUm9VDNvKaknY"
 CREDENTIALS_PATH = "/etc/secrets/service_account.json"
 ROWS_PER_SHEET = 800
